ee1605764e40b3ccf72c1978a39d3567b5b827aa
Margarita <l.m.klingen@gmail.com>
2019-11-19 14:36:35 +0100

    situatie weerbeeld and asset logo

--- app/javascript/controlButtonT72Clicks.js
+++ app/javascript/controlButtonT72Clicks.js
@@ -15,32 +15,24 @@ const controlButtonT72Clicks = () => {
     console.log("situatie excist")
 
     const buttonWeerbeeld = document.querySelector("#weerbeeld");
-    const buttonWind = document.querySelector("#wind");
-    const buttonIWO = document.querySelector("#IWO");
+    const buttonRegios = document.querySelector("#regios");
 
     buttonWeerbeeld.addEventListener("click", event => {
        buttons.forEach(button => {
          button.classList.remove("control-button-active");
       });
-      map.style.backgroundImage = "url('https://i.imgur.com/WZqPDPC.jpg')"; // keringen
+      map.style.backgroundImage = "url('https://i.imgur.com/bRsreRH.png')";
       buttonWeerbeeld.classList.toggle("control-button-active");
     });
 
-    buttonWind.addEventListener("click", event => {
+    buttonRegios.addEventListener("click", event => {
        buttons.forEach(button => {
          button.classList.remove("control-button-active");
       });
-      map.style.backgroundImage = "url('https://i.imgur.com/vX0qK7Y.png')"; // keringen
-      buttonWind.classList.toggle("control-button-active");
+      map.style.backgroundImage = "url('https://i.imgur.com/vX0qK7Y.png')";
+      buttonRegios.classList.toggle("control-button-active");
     });
 
-      buttonIWO.addEventListener("click", event => {
-       buttons.forEach(button => {
-         button.classList.remove("control-button-active");
-      });
-      map.style.backgroundImage = "url('https://i.imgur.com/uRKc1xf.png')"; // keringen
-      buttonIWO.classList.toggle("control-button-active");
-    });
 
   };
 
@@ -84,14 +76,10 @@ const controlButtonT72Clicks = () => {
 
 
 
-
   if(assets){
     console.log("assets excist")
 
     const buttonKeringen = document.querySelector("#keringen");
-    // const buttonKooysluis = document.querySelector("#kooysluis");
-    // const buttonKoopvaderschutsluis = document.querySelector("#koopvaderschutsluis");
-    // const buttonRWZI = document.querySelector("#RWZI");
 
     buttonKeringen.addEventListener("click", event => {
        buttons.forEach(button => {
@@ -102,30 +90,6 @@ const controlButtonT72Clicks = () => {
     });
 
 
-    // buttonKooysluis.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/XcQIRoO.png')"; // RWZI
-    //   buttonKooysluis.classList.toggle("control-button-active");
-    // });
-
-    // buttonKoopvaderschutsluis.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/gQYW68a.png')";
-    //   buttonKoopvaderschutsluis .classList.toggle("control-button-active");
-    // });
-
-    // buttonRWZI.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/QVkmsr1.png')";
-    //   buttonRWZI.classList.toggle("control-button-active");
-    // });
-
   };
 
 
@@ -133,10 +97,7 @@ const controlButtonT72Clicks = () => {
     console.log("hoogte excist")
 
     const buttonGroterDan55meter = document.querySelector("#groterdan55meter");
-    // const buttonGroterDan45meter = document.querySelector("#groterdan45meter");
-    // const buttonGroterDan35meter = document.querySelector("#groterdan35meter");
     const buttonGroterDan25meter = document.querySelector("#groterdan25meter");
-    // const buttonGroterDan2meter = document.querySelector("#groterdan2meter");
     const buttonDroog = document.querySelector("#droog");
 
     buttonGroterDan55meter.addEventListener("click", event => {
@@ -148,22 +109,6 @@ const controlButtonT72Clicks = () => {
     });
 
 
-    // buttonGroterDan45meter.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/kXeXdxh.jpg')";
-    //   buttonGroterDan45meter.classList.toggle("control-button-active");
-    // });
-
-    // buttonGroterDan35meter.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/tYYenB1.jpg')";
-    //   buttonGroterDan35meter.classList.toggle("control-button-active");
-    // });
-
     buttonGroterDan25meter.addEventListener("click", event => {
        buttons.forEach(button => {
          button.classList.remove("control-button-active");
@@ -172,13 +117,6 @@ const controlButtonT72Clicks = () => {
       buttonGroterDan25meter.classList.toggle("control-button-active");
     });
 
-    // buttonGroterDan2meter.addEventListener("click", event => {
-    //    buttons.forEach(button => {
-    //      button.classList.remove("control-button-active");
-    //   });
-    //   map.style.backgroundImage = "url('https://i.imgur.com/duJ3vOo.jpg')";
-    //   buttonGroterDan2meter.classList.toggle("control-button-active");
-    // });
 
     buttonDroog.addEventListener("click", event => {
        buttons.forEach(button => {
@@ -187,7 +125,6 @@ const controlButtonT72Clicks = () => {
       map.style.backgroundImage = "url('https://i.imgur.com/0xyYPaR.jpg')";
       buttonDroog.classList.toggle("control-button-active");
     });
-
 
 
 
