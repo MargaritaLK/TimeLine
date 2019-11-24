dd333f24cac800d0d5ee2ecc32f9a4acbe780e31
Margarita <l.m.klingen@gmail.com>
2019-11-24 10:52:02 +0100

    row changes on opagd t48

--- app/javascript/controlButtonT48Clicks.js
+++ app/javascript/controlButtonT48Clicks.js
@@ -4,15 +4,15 @@ const controlButtonT48Clicks = () => {
   const map = document.querySelector(".control-map");
   const buttons = document.querySelectorAll(".control-button");
 
-  const maxDepth = document.querySelector("#max-depth");
+  const impactWater = document.querySelector("#impact-water");
 
 
-  if (maxDepth) {
+  if (impactWater) {
 
-    console.log("maxDepthexcist")
+    console.log("impactWater")
 
     const buttonS1Pmd = document.querySelector("#S1Pmd")
-    const buttonS2Pmd = document.querySelector("#S2Pmd")
+    // const buttonS2Pmd = document.querySelector("#S2Pmd")
 
     buttonS1Pmd.addEventListener("click", event => {
       buttons.forEach(button => {
@@ -23,13 +23,13 @@ const controlButtonT48Clicks = () => {
     });
 
 
-    buttonS2Pmd.addEventListener("click", event => {
-      buttons.forEach(button => {
-        button.classList.remove("control-button-active");
-      });
-      map.style.backgroundImage = "url('https://i.imgur.com/qfw1GiU.jpg')";
-      buttonS2Pmd.classList.toggle("control-button-active");
-    });
+    // buttonS2Pmd.addEventListener("click", event => {
+    //   buttons.forEach(button => {
+    //     button.classList.remove("control-button-active");
+    //   });
+    //   map.style.backgroundImage = "url('https://i.imgur.com/qfw1GiU.jpg')";
+    //   buttonS2Pmd.classList.toggle("control-button-active");
+    // });
 
 
 
