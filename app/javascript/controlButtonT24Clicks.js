66a366b0bf9e2d50583365a3fec2de2406841ba4
Margarita <l.m.klingen@gmail.com>
2019-11-25 16:41:04 +0100

    wegen in de tijd

--- app/javascript/controlButtonT24Clicks.js
+++ app/javascript/controlButtonT24Clicks.js
@@ -5,6 +5,7 @@ const controlButtonT24Clicks = () => {
   const buttons = document.querySelectorAll(".control-button");
 
   const scen2Pand = document.querySelector("#scen2-pand");
+  const scen2Wegen = document.querySelector("#scen2-wegen");
   const impactS2 = document.querySelector("#impact-S2");
   const todo = document.querySelector("#todo");
 
@@ -15,6 +16,7 @@ const controlButtonT24Clicks = () => {
 
     const buttonS2P1u = document.querySelector("#S2P1u");
     const buttonS2P2u = document.querySelector("#S2P2u");
+    const buttonS2P3u = document.querySelector("#S2P3u");
     const buttonS2P4u = document.querySelector("#S2P4u");
     const buttonS2P6u = document.querySelector("#S2P6u");
     const buttonS2P12u = document.querySelector("#S2P12u");
@@ -36,6 +38,14 @@ const controlButtonT24Clicks = () => {
       });
       map.style.backgroundImage = "url('https://i.imgur.com/lnab2Rp.jpg')";
       buttonS2P2u.classList.toggle("control-button-active");
+    });
+
+    buttonS2P3u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/y19uncu.jpg')";
+      buttonS2P3u.classList.toggle("control-button-active");
     });
 
     buttonS2P4u.addEventListener("click", event => {
@@ -82,6 +92,91 @@ const controlButtonT24Clicks = () => {
   }
 
 
+
+if (scen2Wegen) {
+
+    console.log("panden scen1 excist")
+
+    const buttonS2W1u = document.querySelector("#S2W1u");
+    const buttonS2W2u = document.querySelector("#S2W2u");
+    const buttonS2W3u = document.querySelector("#S2W3u");
+    const buttonS2W4u = document.querySelector("#S2W4u");
+    const buttonS2W6u = document.querySelector("#S2W6u");
+    const buttonS2W12u = document.querySelector("#S2W12u");
+    const buttonS2W24u = document.querySelector("#S2W24u");
+    const buttonS2W38u = document.querySelector("#S2W38u");
+
+
+    buttonS2W1u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/pWZWwgf.jpg')";
+      buttonS2W1u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W2u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/DNsXtKy.jpg')";
+      buttonS2W2u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W3u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/IaeefSJ.jpg')";
+      buttonS2W3u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W4u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/IsuWRUI.jpg')";
+      buttonS2W4u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W6u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/p5s01Jh.jpg')";
+      buttonS2W6u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W12u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/sR1lncX.jpg')";
+      buttonS2W12u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W24u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/gJJPTNT.jpg')";
+      buttonS2W24u.classList.toggle("control-button-active");
+    });
+
+    buttonS2W38u.addEventListener("click", event => {
+      buttons.forEach(button => {
+        button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/klTecXZ.jpg')";
+      buttonS2W38u.classList.toggle("control-button-active");
+    });
+
+
+  }
+
+
+
+
   if (impactS2) {
 
     console.log("impactS2")
@@ -115,8 +210,9 @@ const controlButtonT24Clicks = () => {
     });
 
 
-
   }
+
+
 
 
 
