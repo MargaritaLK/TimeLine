b92758e3555804421fb64f6c3b3ff31708d7fd3d
Margarita <l.m.klingen@gmail.com>
2019-11-26 11:45:38 +0100

    start system page

---
+++ app/javascript/controlButtonSystemClicks.js
@@ -0,0 +1,75 @@
+const controlButtonSystemClicks = () => {
+
+
+  const map = document.querySelector(".control-map");
+  const buttons = document.querySelectorAll(".control-button");
+
+  const systemPropagation1 = document.querySelector("#system-propagation1");
+  const systemCriticalPoint1 = document.querySelector("#system-critical-point1");
+
+
+
+  if(systemPropagation1){
+    console.log("systemModel excist")
+
+    const buttonQloc1_9u30 = document.querySelector("#Qloc1_9u30");
+    const buttonQloc1_9u45 = document.querySelector("#Qloc1_9u45");
+    const buttonQloc1_10u00 = document.querySelector("#Qloc1_10u00");
+
+    buttonQloc1_9u30.addEventListener("click", event => {
+       buttons.forEach(button => {
+         button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/SOdLoIj.png')";
+      buttonQloc1_9u30.classList.toggle("control-button-active");
+    });
+
+    buttonQloc1_9u45.addEventListener("click", event => {
+       buttons.forEach(button => {
+         button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/SSZwsH3.png')";
+      buttonQloc1_9u45.classList.toggle("control-button-active");
+    });
+
+    buttonQloc1_10u00.addEventListener("click", event => {
+       buttons.forEach(button => {
+         button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/0ubMj2R.png')";
+      buttonQloc1_10u00.classList.toggle("control-button-active");
+    });
+
+
+  };
+
+
+
+  if(systemCriticalPoint1){
+    console.log("critical-point1")
+
+    const buttonCP1STopview = document.querySelector("#CP1_topview");
+
+
+    buttonCP1STopview.addEventListener("click", event => {
+       buttons.forEach(button => {
+         button.classList.remove("control-button-active");
+      });
+      map.style.backgroundImage = "url('https://i.imgur.com/rCiRyUx.png')";
+      buttonCP1STopview.classList.toggle("control-button-active");
+    });
+
+
+  };
+
+
+
+
+
+
+
+
+};
+
+
+export {controlButtonSystemClicks}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import {controlButtonT72Clicks} from '../controlButtonT72Clicks';
 import {controlButtonT48Clicks} from '../controlButtonT48Clicks';
 import {controlButtonT24Clicks} from '../controlButtonT24Clicks';
 import {controlButtonDoorbraakClicks} from '../controlButtonDoorbraakClicks';
+import {controlButtonSystemClicks} from '../controlButtonSystemClicks';
 import {activePage} from '../activePage';
 
 
@@ -13,4 +14,5 @@ controlButtonT72Clicks();
 controlButtonT48Clicks();
 controlButtonT24Clicks();
 controlButtonDoorbraakClicks();
+controlButtonSystemClicks();
 activePage();
