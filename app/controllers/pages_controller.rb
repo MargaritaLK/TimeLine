f85795385d4c4b0a5ae974ae8e1ab5c67dbc1f07
Margarita <l.m.klingen@gmail.com>
2021-01-06 17:27:07 +0100

    proberen

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 
   http_basic_authenticate_with name: ENV["USER1"], password: ENV["PASS1"], except: :home
+  # http_basic_authenticate_with name: ENV["USER1"], password: ENV["PASS1"], except: :home :denhelder
 
   def home
   end
